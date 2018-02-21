c5e0948f75229ee4076663fae3589d6668b7306e
linuxwebexpert <Linux.Web.Expert@Gmail.com>
2018-02-21 03:50:19 -0800

    Verified working OTP Dockerfile with port 8888->80

--- otp/otp/otp.sh
+++ otp/otp/otp.sh
@@ -2,4 +2,4 @@
 # Run OTP in standalone mode
 # Standalone OTP can build a graph, visualize a graph, run an OTP API server,
 # or any combination of these.
-java -Xmx6G -Xverify:none -jar /srv/tripplanner/otp/otp-1.2.0-shaded.jar --build /srv/tripplanner/data/ --cache /srv/tripplanner/otp/ned --inMemory --server --port 8888 > /var/log/otp.log &
+java -Xmx6G -Xverify:none -jar /srv/tripplanner/otp/otp-1.0.0-shaded.jar --build /srv/tripplanner/data/ --cache /srv/tripplanner/otp/ned --inMemory --server --port 8888 > /var/log/otp.log &
