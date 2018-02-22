aa5467b13b230372e4ce8e9ec233a4cf43bf8597
linuxwebexpert <Linux.Web.Expert@Gmail.com>
2018-02-22 05:17:46 -0800

    Updated Docker compose and startup scripts

--- apache/docker-startup.sh
+++ apache/docker-startup.sh
@@ -3,6 +3,8 @@
 set -exu
 
 service apache2 start
+service ntp start
+service cron start
 
 # start the ssh daemon
 /usr/sbin/sshd -D
--- otp/docker-startup.sh
+++ otp/docker-startup.sh
@@ -2,15 +2,15 @@
 
 set -exu
 
-# start docker
-#/etc/init.d/docker start
-
-# Load OSM and GTFS data
+# Start OTP
+service ntp start
+service cron start
+service supervisor start
+# Load OSM and GTFS data and Supervisord will start when it's done building Graph.obj
 /srv/tripplanner/gtfs-manager/gtfs-loader.sh
 
-# Start OTP
-#/srv/tripplanner/otp/otp.sh
-/usr/bin/supervisorctl restart vta:vta_otp
+/usr/bin/supervisorctl restart vta:vta_otp_monitor
+
 
 # start the ssh daemon
 /usr/sbin/sshd -D
