363d36f59c4b1b2f261d8842b1f81b8736ae85f0
Markes <Markes_B@vta.org>
2018-03-13 08:24:03 -0700

    Added OSM pull to build to speed up deployment time.

--- otp/docker-startup.sh
+++ otp/docker-startup.sh
@@ -6,11 +6,8 @@ set -exu
 service ntp start
 service cron start
 service supervisor start
-# Load OSM and GTFS data and Supervisord will start when it's done building Graph.obj
-/srv/tripplanner/gtfs-manager/gtfs-loader.sh
 
 /usr/bin/supervisorctl restart vta:vta_otp_monitor
-
 
 # start the ssh daemon
 /usr/sbin/sshd -D
